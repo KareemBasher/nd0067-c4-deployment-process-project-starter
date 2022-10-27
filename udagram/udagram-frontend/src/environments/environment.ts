eace13ea21c6ed60876b0ba4e744e94476616b41
Karim.basher <kareem.basher@gmail.com>
2022-10-27 18:32:18 +0200

    changed API URL.

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: 'Udagram',
   // apiHost: 'http://localhost:8080/api/v0'
-  apiHost: 'http://udagram-api-dev.eba-cc6rqj9b.us-east-1.elasticbeanstalk.com/'
+  apiHost: 'http://udagram-api-dev.eba-cc6rqj9b.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
